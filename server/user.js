fccd62360a11c80c30914f80379e0b03d125c6b8
gmw-zjw <gaomingwei2017@163.com>
2018-03-29 10:31:10 +0800

    login

--- server/user.js
+++ server/user.js
@@ -4,48 +4,48 @@ const utility = require('utility')
 const models = require('./model')
 const User = models.getModel('user')
 
-Router.get('/list', function(req, res) {
+Router.get('/list', function (req, res) {
     // 删除所有用户
     // User.remove({}, function(err, doc) {})
     // 查找
-    User.find({}, function(err, doc) {
+    User.find({}, function (err, doc) {
         return res.json(doc)
     })
 })
 // POST
 Router.post('/register', function (req, res) {
     console.log(req.body)
-    const {user, pwd, type} = req.body
+    const { user, pwd, type } = req.body
     const sodle = '12nhjhsj_nkjn@#$nk>...nu!!``~~~'
-    User.findOne({user}, function(err, doc) {
+    User.findOne({ user }, function (err, doc) {
         if (doc) {
-            return res.json({code: 1, msg: '用户名重复'})
+            return res.json({ code: 1, msg: '用户名重复' })
         }
-        User.create({user, pwd: utility.md5(pwd + sodle), type}, function(e, d) {
+        User.create({ user, pwd: utility.md5(pwd + sodle), type }, function (e, d) {
             if (e) {
-                return res.json({code: 1, msg: '服务器出错'})
+                return res.json({ code: 1, msg: '服务器出错' })
             }
-            return res.json({code: 0})
+            return res.json({ code: 0 })
         })
     })
 })
 
 // post login
-Router.post('/login', function(req,res){
-	const {user, pwd} = req.body
-	User.findOne({user,pwd:md5Pwd(pwd)},_filter,function(err,doc){
-		if (!doc) {
-			return res.json({code:1,msg:'用户名或者密码错误'})
-		}
-		res.cookie('userid', doc._id)
-		return res.json({code:0,data:doc})
-	})
+Router.post('/login', function (req, res) {
+    const { user, pwd } = req.body
+    User.findOne({ user, pwd: md5Pwd(pwd) }, _filter, function (err, doc) {
+        if (!doc) {
+            return res.json({ code: 1, msg: '用户名或者密码错误' })
+        }
+        res.cookie('userid', doc._id)
+        return res.json({ code: 0, data: doc })
+    })
 })
 
 // user
-Router.get('/info', function(req, res) {
+Router.get('/info', function (req, res) {
     // 通过cookie校验用户是否登录
-    return res.json({code: 1})
+    return res.json({ code: 1 })
 })
 
 module.exports = Router
--- src/container/login/Login.js
+++ src/container/login/Login.js
@@ -1,72 +1,63 @@
 import React from 'react'
 import Logo from '../../conponent/logo/Logo'
-import {
-    Button,
-    List,
-    WingBlank,
-    WhiteSpace,
-    InputItem
-} from 'antd-mobile'
+import {List, InputItem, WingBlank, WhiteSpace, Button} from 'antd-mobile'
 import { connect } from 'react-redux'
-import { login } from '../../redux/user.redux'
+import {Redirect} from 'react-router-dom'
+import {login} from '../../redux/user.redux'
 
 
 @connect(
-    state => state.user,
-    { login }
+	state=>state.user,
+	{login}
 )
-class Login extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            user: '',
-            ped: ''
-        }
+class Login extends React.Component{
+	constructor(props) {
+		super(props)
+		this.state = {
+			user:'',
+			pwd:''
+		}
+		this.register = this.register.bind(this)
+		this.handleLogin = this.handleLogin.bind(this)
+	}
+	register(){
+		this.props.history.push('/register')
+	}
+	handleChange(key,val){
+		this.setState({
+			[key]:val
+		})
+	}
+	handleLogin(){
+		this.props.login(this.state)
+	}
+	render(){
+		return (
+			<div>
+				{this.props.redirectTo? <Redirect to={this.props.redirectTo} />:null}
+				<Logo></Logo>
+				<WingBlank>
+					<List>
+						{this.props.msg?<p className='error-msg'>{this.props.msg}</p>:null}
+						<InputItem
+							onChange={v=>this.handleChange('user',v)}
 
-        this.Register = this.Register.bind(this)
-        this.handleLogin = this.handleLogin.bind(this)
-    }
-    // 注册 
-    Register() {
-        console.log(this.props)
-        this.props.history.push('/register')
-    }
-
-    handleChange(key, val) {
-        this.setState({
-            [key]: val
-        })
-    }
-
-    // 登录
-    handleLogin() {
-           this.props.login(this.state)
-           console.log(this.state)
-        // console.log(this.props)
-    }
-
-    render() {
-        return (
-            <div>
-                <Logo />
-                <WingBlank>
-                    <List>
-                        <InputItem
-                            onChange={v=> this.handleChange('user', v)}
-                        >用户名</InputItem>
-                        <WhiteSpace />
-                        <InputItem
+						>用户</InputItem>
+						<WhiteSpace />
+						<InputItem
                             type="password"
-                            onChange={v=> this.handleChange('pwd', v)}
-                        >密码</InputItem>
-                    </List>
-                    <Button onClick={this.handleLogin} type="primary">登录</Button>
-                    <WhiteSpace />
-                    <Button type="primary" onClick={this.Register}>注册</Button>
-                </WingBlank>
-            </div>
-        );
-    }
+							onChange={v=>this.handleChange('pwd',v)}
+
+						>密码</InputItem>
+					</List>
+					<WhiteSpace />
+					<Button onClick={this.handleLogin} type='primary'>登录</Button>
+					<WhiteSpace />
+					<Button onClick={this.register} type='primary'>注册</Button>
+				</WingBlank>
+			</div>
+		)
+	}
 }
 
 export default Login
