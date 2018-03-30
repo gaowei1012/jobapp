8f9daa2ef3635f3ca6722071133809b132a3052a
gmw-zjw <gaomingwei2017@163.com>
2018-03-30 11:34:57 +0800

    底部导航完善

--- src/conponent/navlink/NavLinkBar.js
+++ src/conponent/navlink/NavLinkBar.js
@@ -2,14 +2,16 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {TabBar} from 'antd-mobile'
+import {withRouter} from 'react-router-dom'
 
+@withRouter
 class NavLinkBar extends React.Component {
     static PropTypes = {
         data: PropTypes.array
     }
     render() {
         const navList = this.props.data.filter(v=>!v.hide)
-        console.log(navList)
+        const {pathname} = this.props.location
         return (
             <TabBar>
               {navList.map(v=>(
@@ -17,6 +19,11 @@ class NavLinkBar extends React.Component {
                     key={v.path}
                     title={v.text}
                     icon={{uri: require(`./img/${v.icon}.png`)}}
+                    selectedIcon={{uri: require(`./img/${v.icon}-active.png`)}}
+                    selected={pathname === v.path}
+                    onPress={()=> {
+                        this.props.history.push(v.path)
+                    }}
                   ></TabBar.Item>
               ))}
             </TabBar>
